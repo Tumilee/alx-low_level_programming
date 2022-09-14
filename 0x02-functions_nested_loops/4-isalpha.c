ccb7d02d9f61980d094129b4113700260765c2d8
Tumie <tumierandara@gmail.com>
2022-09-14 13:43:51 -0700

    Hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,15 @@
 
 /**
  * _isalpha - Checks for alphabetical order
- * @c: a character to be checked
+ * @c: a character to be checked on
  * Return: returns 0 or 1 depending on condition
  */
 int _isalpha(int c)
 {
-	if (isalpha(c))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
 	else
-	{
 		return (0);
-	}
 }

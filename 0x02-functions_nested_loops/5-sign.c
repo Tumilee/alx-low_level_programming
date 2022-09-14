c2ce2279e45c2440c76d8b70f611fbb566b29410
Tumie <tumierandara@gmail.com>
2022-09-14 13:49:51 -0700

    Fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign -> prints sign based on condition
  * @n: argument passed
- * return: 1, 0, (-1)
+ * Return: 0
  */
 int print_sign(int n)
 {
@@ -12,14 +12,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return ('0');
+		_putchar('-');
+		return ('-1');
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 }

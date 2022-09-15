547ca943e88a4ba9f5470eac4f1106f6b32f5cfd
Tumie <tumierandara@gmail.com>
2022-09-15 15:29:12 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,29 +6,12 @@
  * main - Testsfunction that prints if integer is positive or negative
  * Return: 0
  */
-void positive_or_negative(int i)
-{
-	int n;
 
-	srand(time(0));
-	n = i;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
-/**
- * main - debugging
- * return: 0
- */
-
+int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98 % 2;
 	positive_or_negative(i);
 
 	return (0);

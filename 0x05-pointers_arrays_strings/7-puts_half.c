f89d2d9625855210d6241b5402a3e53110761ae1
Tumie <tumierandara@gmail.com>
2022-09-21 06:03:45 -0700

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,61 +10,23 @@
 
 void puts_half(char *str)
 {
-	int i, len = _strlen(str);
+	int i;
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
-}
+	for (i = 0; str[i])
+		i++;
 
-/**
- * _strlen - returns the length of a string
- * @s: string
- *
- * Return: the length of the given string
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
-}
-#include <stdio.h>
-#include "main.h"
-
-/**
- * puts_half - prints a string
- * @str: string to print
- *
- * Return: nothing
- */
-
-void puts_half(char *str)
-{
-	int i, len = _strlen(str);
-
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string
- *
- * Return: the length of the given string
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
+	if ((i % 2) == 0)
+	{
+		i = i / 2;
+	}
+	else
+	{
+		i = (i + 1) / 2;
+	}
+	while (*(str + 1))
+	{
+		_putchar(str[i]);
+			i++;
+	}
+	_putchar('\n');
 }

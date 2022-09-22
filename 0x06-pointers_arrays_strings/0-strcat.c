1740e3cec806fb09ded2666623f6307b0f0e77ca
Tumie <tumierandara@gmail.com>
2022-09-22 08:31:34 -0700

    Links two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
 
-	i = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0
+	while (dest[index++])
+		dest_len++;
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
